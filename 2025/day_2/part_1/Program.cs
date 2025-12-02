10e3460b5cccbd79fb015c23e950a76bc063090e
Daniel Palma Garcia <danpalma94@gmail.com>
2025-12-02 00:03:53 -0600

    2025 - day 2 part 1

---
+++ 2025/day_2/part_1/Program.cs
@@ -0,0 +1,24 @@
+﻿string filePath = "input.txt";
+string text = File.ReadAllText(filePath);
+string[] ranges = text.Split(',');
+long sum_invalid = 0;
+foreach (string range in ranges)
+{
+    Console.WriteLine(range);
+    long bottomRange = Convert.ToInt64(range.Split('-')[0]);
+    long topRange = Convert.ToInt64(range.Split('-')[1]);
+    for (long i = bottomRange ; i < topRange + 1 ; i++)
+    {
+        string str = Convert.ToString(i);
+        if (str.Length % 2 == 0) 
+        {
+            int half = str.Length / 2;
+            if (str[0..half] == str[half..])
+            {
+                Console.WriteLine(str);
+                sum_invalid += Convert.ToInt64(str);
+            }
+        }
+    }
+}
+Console.WriteLine("Sum of invalid IDs: {0}", sum_invalid);
