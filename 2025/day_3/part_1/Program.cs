bd78c09d24b30bc2f9e9ff2449e5c8032a2a3dcb
Daniel Palma Garcia <danpalma94@gmail.com>
2025-12-03 01:46:44 -0600

    2025 - day 3 part 1

---
+++ 2025/day_3/part_1/Program.cs
@@ -0,0 +1,25 @@
+﻿string filePath = "input.txt";
+string text = File.ReadAllText(filePath);
+string[] banks = text.Split('\n');
+int sum_joltage = 0;
+foreach (string bank in banks)
+{
+    int highest = 0;
+    for (int i = 0 ; i < bank.Length ; i++)
+    {
+        int tens = (int)char.GetNumericValue(bank[i]) * 10;
+        if (tens >= highest)
+        {
+            for (int j = i+1 ; j < bank.Length ; j++)
+            {
+                int unit = (int)char.GetNumericValue(bank[j]);
+                if (tens+unit > highest)
+                {
+                    highest = tens+unit;
+                }
+            }
+        }
+    }
+    sum_joltage += highest;
+}
+Console.WriteLine($"The sum is {sum_joltage}");
